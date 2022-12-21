229066cbf7c6ae0f4d5c0ac34c1e35a331dded0b
Austin Horstman <khaneliman12@gmail.com>
2022-12-21 09:13:21 -0600

    macOS - Updated hammerspoon config

--- macos/yabai/home/.hammerspoon/Spoons/MiroWindowsManager.spoon/init.lua
+++ macos/yabai/home/.hammerspoon/Spoons/MiroWindowsManager.spoon/init.lua
@@ -1,6 +1,6 @@
 -- Copyright (c) 2018 Miro Mannino
--- Permission is hereby granted, free of charge, to any person obtaining a copy of this 
--- software and associated documentation files (the "Software"), to deal in the Software 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this
+-- software and associated documentation files (the "Software"), to deal in the Software
 -- without restriction, including without limitation the rights to use, copy, modify, merge,
 -- publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 -- to whom the Software is furnished to do so, subject to the following conditions:
@@ -8,23 +8,21 @@
 -- The above copyright notice and this permission notice shall be included in all copies
 -- or substantial portions of the Software.
 --
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
--- INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+-- INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 -- PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
--- FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
--- OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+-- FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+-- OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 -- DEALINGS IN THE SOFTWARE.
-
 --- === MiroWindowsManager ===
 ---
 --- With this script you will be able to move the window in halves and in corners using your keyboard and mainly using arrows. You would also be able to resize them by thirds, quarters, or halves.
---- 
+---
 --- Official homepage for more info and documentation: [https://github.com/miromannino/miro-windows-manager](https://github.com/miromannino/miro-windows-manager)
 ---
 --- Download: [https://github.com/miromannino/miro-windows-manager/raw/master/MiroWindowsManager.spoon.zip](https://github.com/miromannino/miro-windows-manager/raw/master/MiroWindowsManager.spoon.zip)
 ---
-
-local obj={}
+local obj = {}
 obj.__index = obj
 
 -- Metadata
@@ -36,36 +34,31 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 --- MiroWindowsManager.sizes
 --- Variable
---- The sizes that the window can have. 
+--- The sizes that the window can have.
 --- The sizes are expressed as dividend of the entire screen's size.
 --- For example `{2, 3, 3/2}` means that it can be 1/2, 1/3 and 2/3 of the total screen's size
-obj.sizes = {2, 3, 3/2}
+obj.sizes = { 2, 3, 3 / 2 }
 
 --- MiroWindowsManager.fullScreenSizes
 --- Variable
---- The sizes that the window can have in full-screen. 
+--- The sizes that the window can have in full-screen.
 --- The sizes are expressed as dividend of the entire screen's size.
 --- For example `{1, 4/3, 2}` means that it can be 1/1 (hence full screen), 3/4 and 1/2 of the total screen's size
-obj.fullScreenSizes = {1, 4/3, 2}
+obj.fullScreenSizes = { 1, 4 / 3, 2 }
 
 --- MiroWindowsManager.GRID
 --- Variable
 --- The screen's size using `hs.grid.setGrid()`
 --- This parameter is used at the spoon's `:init()`
-obj.GRID = {w = 24, h = 24}
+obj.GRID = { w = 24, h = 24 }
 
-obj._pressed = {
-  up = false,
-  down = false,
-  left = false,
-  right = false
-}
+obj._pressed = { up = false, down = false, left = false, right = false }
 
 function obj:_nextStep(dim, offs, cb)
   if hs.window.focusedWindow() then
-    local axis = dim == 'w' and 'x' or 'y'
-    local oppDim = dim == 'w' and 'h' or 'w'
-    local oppAxis = dim == 'w' and 'y' or 'x'
+    local axis = dim == "w" and "x" or "y"
+    local oppDim = dim == "w" and "h" or "w"
+    local oppAxis = dim == "w" and "y" or "x"
     local win = hs.window.frontmostWindow()
     local id = win:id()
     local screen = win:screen()
@@ -73,17 +66,18 @@ function obj:_nextStep(dim, offs, cb)
     cell = hs.grid.get(win, screen)
 
     local nextSize = self.sizes[1]
-    for i=1,#self.sizes do
+    for i = 1, #self.sizes do
       if cell[dim] == self.GRID[dim] / self.sizes[i] and
-        (cell[axis] + (offs and cell[dim] or 0)) == (offs and self.GRID[dim] or 0)
-        then
-          nextSize = self.sizes[(i % #self.sizes) + 1]
+          (cell[axis] + (offs and cell[dim] or 0)) ==
+          (offs and self.GRID[dim] or 0) then
+        nextSize = self.sizes[(i % #self.sizes) + 1]
         break
       end
     end
 
     cb(cell, nextSize)
-    if cell[oppAxis] ~= 0 and cell[oppAxis] + cell[oppDim] ~= self.GRID[oppDim] then
+    if cell[oppAxis] ~= 0 and cell[oppAxis] + cell[oppDim] ~=
+        self.GRID[oppDim] then
       cell[oppDim] = self.GRID[oppDim]
       cell[oppAxis] = 0
     end
@@ -101,11 +95,12 @@ function obj:_nextFullScreenStep()
     cell = hs.grid.get(win, screen)
 
     local nextSize = self.fullScreenSizes[1]
-    for i=1,#self.fullScreenSizes do
-      if cell.w == self.GRID.w / self.fullScreenSizes[i] and 
-         cell.h == self.GRID.h / self.fullScreenSizes[i] and
-         cell.x == (self.GRID.w - self.GRID.w / self.fullScreenSizes[i]) / 2 and
-         cell.y == (self.GRID.h - self.GRID.h / self.fullScreenSizes[i]) / 2 then
+    for i = 1, #self.fullScreenSizes do
+      if cell.w == self.GRID.w / self.fullScreenSizes[i] and cell.h ==
+          self.GRID.h / self.fullScreenSizes[i] and cell.x ==
+          (self.GRID.w - self.GRID.w / self.fullScreenSizes[i]) / 2 and
+          cell.y == (self.GRID.h - self.GRID.h / self.fullScreenSizes[i]) /
+          2 then
         nextSize = self.fullScreenSizes[(i % #self.fullScreenSizes) + 1]
         break
       end
@@ -120,16 +115,6 @@ function obj:_nextFullScreenStep()
   end
 end
 
-function obj:_moveNextScreenStep()
-  if hs.window.focusedWindow() then
-    local win = hs.window.frontmostWindow()
-    local id = win:id()
-    local screen = win:screen()
-
-    win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
-  end
-end
-
 function obj:_fullDimension(dim)
   if hs.window.focusedWindow() then
     local win = hs.window.frontmostWindow()
@@ -137,11 +122,11 @@ function obj:_fullDimension(dim)
     local screen = win:screen()
     cell = hs.grid.get(win, screen)
 
-    if (dim == 'x') then
-      cell = '0,0 ' .. self.GRID.w .. 'x' .. self.GRID.h
-    else  
+    if dim == "x" then
+      cell = "0,0 " .. self.GRID.w .. "x" .. self.GRID.h
+    else
       cell[dim] = self.GRID[dim]
-      cell[dim == 'w' and 'x' or 'y'] = 0
+      cell[dim == "w" and "x" or "y"] = 0
     end
 
     hs.grid.set(win, cell, screen)
@@ -158,7 +143,6 @@ end
 ---   * down: for the down action (usually {hyper, "down"})
 ---   * left: for the left action (usually {hyper, "left"})
 ---   * fullscreen: for the full-screen action (e.g. {hyper, "f"})
----   * nextscreen: for the multi monitor next screen action (e.g. {hyper, "n"})
 ---
 --- A configuration example can be:
 --- ```
@@ -169,84 +153,69 @@ end
 ---   down = {hyper, "down"},
 ---   left = {hyper, "left"},
 ---   fullscreen = {hyper, "f"}
----   nextscreen = {hyper, "n"}
 --- })
 --- ```
 function obj:bindHotkeys(mapping)
   hs.inspect(mapping)
   print("Bind Hotkeys for Miro's Windows Manager")
 
-  hs.hotkey.bind(mapping.down[1], mapping.down[2], function ()
+  hs.hotkey.bind(mapping.down[1], mapping.down[2], function()
     self._pressed.down = true
-    if self._pressed.up then 
-      self:_fullDimension('h')
+    if self._pressed.up then
+      self:_fullDimension("h")
     else
-      self:_nextStep('h', true, function (cell, nextSize)
+      self:_nextStep("h", true, function(cell, nextSize)
         cell.y = self.GRID.h - self.GRID.h / nextSize
         cell.h = self.GRID.h / nextSize
       end)
     end
-  end, function () 
-    self._pressed.down = false
-  end)
+  end, function() self._pressed.down = false end)
 
-  hs.hotkey.bind(mapping.right[1], mapping.right[2], function ()
+  hs.hotkey.bind(mapping.right[1], mapping.right[2], function()
     self._pressed.right = true
-    if self._pressed.left then 
-      self:_fullDimension('w')
+    if self._pressed.left then
+      self:_fullDimension("w")
     else
-      self:_nextStep('w', true, function (cell, nextSize)
+      self:_nextStep("w", true, function(cell, nextSize)
         cell.x = self.GRID.w - self.GRID.w / nextSize
         cell.w = self.GRID.w / nextSize
       end)
     end
-  end, function () 
-    self._pressed.right = false
-  end)
+  end, function() self._pressed.right = false end)
 
-  hs.hotkey.bind(mapping.left[1], mapping.left[2], function ()
+  hs.hotkey.bind(mapping.left[1], mapping.left[2], function()
     self._pressed.left = true
-    if self._pressed.right then 
-      self:_fullDimension('w')
+    if self._pressed.right then
+      self:_fullDimension("w")
     else
-      self:_nextStep('w', false, function (cell, nextSize)
+      self:_nextStep("w", false, function(cell, nextSize)
         cell.x = 0
         cell.w = self.GRID.w / nextSize
       end)
     end
-  end, function () 
-    self._pressed.left = false
-  end)
+  end, function() self._pressed.left = false end)
 
-  hs.hotkey.bind(mapping.up[1], mapping.up[2], function ()
+  hs.hotkey.bind(mapping.up[1], mapping.up[2], function()
     self._pressed.up = true
-    if self._pressed.down then 
-        self:_fullDimension('h')
+    if self._pressed.down then
+      self:_fullDimension("h")
     else
-      self:_nextStep('h', false, function (cell, nextSize)
+      self:_nextStep("h", false, function(cell, nextSize)
         cell.y = 0
         cell.h = self.GRID.h / nextSize
       end)
     end
-  end, function () 
-    self._pressed.up = false
-  end)
+  end, function() self._pressed.up = false end)
 
-  hs.hotkey.bind(mapping.fullscreen[1], mapping.fullscreen[2], function ()
-    self:_nextFullScreenStep()
-  end)
-
-  hs.hotkey.bind(mapping.nextscreen[1], mapping.nextscreen[2], function ()
-    self:_moveNextScreenStep()
-  end)
-
+  hs.hotkey.bind(mapping.fullscreen[1], mapping.fullscreen[2],
+    function() self:_nextFullScreenStep() end)
 end
 
 function obj:init()
   print("Initializing Miro's Windows Manager")
-  hs.grid.setGrid(obj.GRID.w .. 'x' .. obj.GRID.h)
-  hs.grid.MARGINX = 0
-  hs.grid.MARGINY = 0
+  hs.grid.setGrid(obj.GRID.w .. "x" .. obj.GRID.h)
+  hs.grid.MARGINX = 10
+  hs.grid.MARGINY = 25
 end
 
 return obj
--- macos/yabai/home/.hammerspoon/init.lua
+++ macos/yabai/home/.hammerspoon/init.lua
@@ -1,24 +1,38 @@
-local super = {"ctrl", "alt", "cmd"}
-local sl = require("smartLaunch")
+local hyper = { "ctrl", "alt", "cmd" }
+require("smartLaunch")
 
 hs.loadSpoon("MiroWindowsManager")
 
 hs.window.animationDuration = 0.3
 spoon.MiroWindowsManager:bindHotkeys({
-  up = {super, "up"},
-  right = {super, "right"},
-  down = {super, "down"},
-  left = {super, "left"},
-  fullscreen = {super, "f"},
-  nextscreen = {super, "n"}
+  up = { hyper, "up" },
+  right = { hyper, "right" },
+  down = { hyper, "down" },
+  left = { hyper, "left" },
+  fullscreen = { hyper, "f" },
+  nextscreen = { hyper, "n" }
 })
 
-require "slowq"    -- Avoid accidental Cmd-Q
+require("slowq") -- Avoid accidental Cmd-Q
 
-hs.hotkey.bind(super, 'P', function()
-  hs.openPreferences()
-end)
+hs.hotkey.bind(hyper, "P", function() hs.openPreferences() end)
+hs.hotkey.bind(hyper, "R", function() hs.reload() end)
 
-hs.hotkey.bind(super, 'R', function()
-  hs.reload()
-end)
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+hs.loadSpoon("ClipboardTool")
+spoon.ClipboardTool:start()
+
+hs.loadSpoon("ColorPicker")
+spoon.ColorPicker:start()
+spoon.ColorPicker:bindHotkeys({ show = { hyper, "C" } })
+
+hs.loadSpoon("Commander")
+-- spoon.Commander:start()
+
+hs.loadSpoon("EjectMenu")
+spoon.EjectMenu:start()
+
+hs.loadSpoon("KSheet")
+spoon.KSheet:bindHotkeys({ toggle = { hyper, "K" } })
