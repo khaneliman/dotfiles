6634d2724e3ac4aa4b9125be2c32f698d4e55f17
Austin Horstman <khaneliman12@gmail.com>
2022-12-23 00:21:00 -0600

    macOS Add paths to fish config

--- dots/macos/yabai/home/.config/fish/config.fish
+++ dots/macos/yabai/home/.config/fish/config.fish
@@ -1,3 +1,6 @@
+fish_add_path  "/opt/local/bin" "/opt/local/sbin"
+eval $("/opt/homebrew/bin/brew" shellenv)
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
