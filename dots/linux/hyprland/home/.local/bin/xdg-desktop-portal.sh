1063e0b774cf9e6486b36b3baf2319fd8c4b98d9
Austin Horstman <khaneliman12@gmail.com>
2023-01-03 08:55:20 -0600

    Hyprland fix file chooser

--- dots/linux/hyprland/home/.local/bin/xdg-desktop-portal.sh
+++ dots/linux/hyprland/home/.local/bin/xdg-desktop-portal.sh
@@ -3,12 +3,15 @@
 sleep 1
 
 killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 
 /usr/libexec/xdg-desktop-portal-hyprland &
 
 sleep 2
 
-/usr/lib/xdg-desktop-portal &
+/usr/lib/xdg-desktop-portal-gtk &
 
+sleep 2
+
+/usr/lib/xdg-desktop-portal &
