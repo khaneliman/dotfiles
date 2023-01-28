81619d6d39ae2d4d8a8867271d859477dc01e99e
Austin Horstman <khaneliman12@gmail.com>
2023-01-28 14:34:09 -0600

    fish update cd function

--- dots/shared/home/.config/fish/functions/cd.fish
+++ dots/shared/home/.config/fish/functions/cd.fish
@@ -1,10 +1,26 @@
 function cd --description 'change directory and run onefetch if a git directory' 
-    if test -n "$argv"
-        builtin cd "$argv";
-    else 
-        builtin cd;
+    set -l parent
+    
+    # navigate to root of git repo
+    if [ $argv = ":/" ]
+        set -l top  (command git rev-parse --show-cdup)
+        builtin cd $top;
+    # check if cd to file and route to parent
+    else if [ -f $argv ] && [ -e $argv ] 
+        set parent (dirname $argv)
+        if [ "$parent" != . ] && [ -d "$parent" ]
+            builtin cd "$parent";
+        end
+    # if navigating to a directory, cd as usual
+    else if [ -d $argv ]
+        if test -n "$argv" && 
+            builtin cd "$argv";
+        else 
+            builtin cd;
+        end
     end
 
+    # check for git information
     git rev-parse 2>/dev/null;
 
     if test $status -eq 0
