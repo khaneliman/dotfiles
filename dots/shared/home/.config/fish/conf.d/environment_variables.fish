ff7095dbd6ba559a3c6cc39cc284606f745b776d
Austin Horstman <khaneliman12@gmail.com>
2023-01-28 13:37:53 -0600

    fish separate environment variables

---
+++ dots/shared/home/.config/fish/conf.d/environment_variables.fish
@@ -0,0 +1,36 @@
+# Hide welcome message
+set -g fish_greeting
+
+set -g theme_nerd_fonts yes
+
+# Exports
+set -g LANG "en_US.UTF-8"
+set -g LC_ALL "en_US.UTF-8"
+set -g VISUAL "nvim"
+set -g EDITOR "$VISUAL"
+set -g MICRO_TRUECOLOR "1"
+set -g GIT_EDITOR 'nvim'
+
+# Git config
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showupstream informative
+set -g __fish_git_prompt_showdirtystate yes
+set -g __fish_git_prompt_char_stateseparator ' '
+set -g __fish_git_prompt_char_cleanstate '✔'
+set -g __fish_git_prompt_char_dirtystate '✚'
+set -g __fish_git_prompt_char_invalidstate '✖'
+set -g __fish_git_prompt_char_stagedstate '●'
+set -g __fish_git_prompt_char_stashstate '⚑'
+set -g __fish_git_prompt_char_untrackedfiles '?'
+set -g __fish_git_prompt_char_upstream_ahead ''
+set -g __fish_git_prompt_char_upstream_behind ''
+set -g __fish_git_prompt_char_upstream_diverged 'ﱟ'
+set -g __fish_git_prompt_char_upstream_equal ''
+set -g __fish_git_prompt_char_upstream_prefix ''''
+
+# Fish colors
+set -g fish_color_command --bold green
+set -g fish_color_error red
+set -g fish_color_quote yellow
+set -g fish_color_param white
+set -g fish_pager_color_selected_completion blue
--- dots/shared/home/.config/fish/config.fish
+++ dots/shared/home/.config/fish/config.fish
@@ -12,35 +12,6 @@ set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
 
-# Hide welcome message
-set -g fish_greeting
-
-set -g theme_nerd_fonts yes
-
-# Fish colors
-set -g fish_color_command --bold green
-set -g fish_color_error red
-set -g fish_color_quote yellow
-set -g fish_color_param white
-set -g fish_pager_color_selected_completion blue
-
-# Git config
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_showupstream informative
-set -g __fish_git_prompt_showdirtystate yes
-set -g __fish_git_prompt_char_stateseparator ' '
-set -g __fish_git_prompt_char_cleanstate '✔'
-set -g __fish_git_prompt_char_dirtystate '✚'
-set -g __fish_git_prompt_char_invalidstate '✖'
-set -g __fish_git_prompt_char_stagedstate '●'
-set -g __fish_git_prompt_char_stashstate '⚑'
-set -g __fish_git_prompt_char_untrackedfiles '?'
-set -g __fish_git_prompt_char_upstream_ahead ''
-set -g __fish_git_prompt_char_upstream_behind ''
-set -g __fish_git_prompt_char_upstream_diverged 'ﱟ'
-set -g __fish_git_prompt_char_upstream_equal ''
-set -g __fish_git_prompt_char_upstream_prefix ''''
-
 # Replace ls with lsd
 [ $(command -v lsd) ] && alias ls='lsd -al --color=always --group-directories-first' # preferred listing
 [ $(command -v lsd) ] && alias la='lsd -a --color=always --group-directories-first'  # all files and dirs
@@ -152,13 +123,6 @@ switch (uname)
             echo Hi, stranger!
 end
 
-# Exports
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export VISUAL="nvim"
-export EDITOR="$VISUAL"
-export MICRO_TRUECOLOR="1"
-
 # SSH setup 
 [ $(command -v fish_ssh_agent) ] && fish_ssh_agent
-load_ssh # uncomment if you need ssh loading keys on shell
+load_ssh # if you need ssh loading keys on shell launch
