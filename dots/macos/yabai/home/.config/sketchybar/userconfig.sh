447f214debe373630f8cd499526c4bc2b1461cac
Austin Horstman <khaneliman12@gmail.com>
2023-02-10 17:32:36 -0600

    macos fix sketchybar music config

--- dots/macos/yabai/home/.config/sketchybar/userconfig.sh
+++ dots/macos/yabai/home/.config/sketchybar/userconfig.sh
@@ -24,6 +24,8 @@ export P_DYNAMIC_ISLAND_APPSWITCH_ENABLED=1
 export P_DYNAMIC_ISLAND_NOTIFICATION_ENABLED=0
 export P_DYNAMIC_ISLAND_VOLUME_ENABLED=1
 export P_DYNAMIC_ISLAND_BRIGHTNESS_ENABLED=1
+export P_DYNAMIC_ISLAND_WIFI_ENABLED=1
+export P_DYNAMIC_ISLAND_POWER_ENABLED=1
 
 # Notch Size
 export P_DYNAMIC_ISLAND_MAX_HEIGHT=90
@@ -32,12 +34,12 @@ export P_DYNAMIC_ISLAND_DEFAULT_WIDTH=185
 export P_DYNAMIC_ISLAND_DEFAULT_CORNER_RADIUS=12
 
 # Animation Settings
-export P_DYNAMIC_ISLAND_SQUISH_AMOUNT=7
+export P_DYNAMIC_ISLAND_SQUISH_AMOUNT=9
 
 #
 ## App Switch Island config
 #
-export P_DYNAMIC_ISLAND_APPSWITCH_EXPAND_WIDTH=150 # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_APPSWITCH_EXPAND_WIDTH=150     # This value should match the notch size, appearence wise
 export P_DYNAMIC_ISLAND_APPSWITCH_MAX_EXPAND_WIDTH=200 # Max size when expanded
 export P_DYNAMIC_ISLAND_APPSWITCH_EXPAND_HEIGHT=65
 export P_DYNAMIC_ISLAND_APPSWITCH_CORNER_RAD=15 # Corner Radius
@@ -46,9 +48,9 @@ export P_DYNAMIC_ISLAND_APPSWITCH_ICON_SIZE=0.4
 #
 ## Volume Island config
 #
-export P_DYNAMIC_ISLAND_VOLUME_EXPAND_WIDTH=192 # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_VOLUME_EXPAND_WIDTH=192     # This value should match the notch size, appearence wise
 export P_DYNAMIC_ISLAND_VOLUME_MAX_EXPAND_WIDTH=250 # Max size when expanded
-export P_DYNAMIC_ISLAND_VOLUME_DEFAULT_HEIGHT=13
+export P_DYNAMIC_ISLAND_VOLUME_DEFAULT_HEIGHT=12
 export P_DYNAMIC_ISLAND_VOLUME_EXPAND_HEIGHT=16
 export P_DYNAMIC_ISLAND_VOLUME_CORNER_RAD=12 # Corner Radius
 export P_DYNAMIC_ISLAND_VOLUME_NORMAL_ICON_COLOR=0xffffffff
@@ -60,9 +62,9 @@ export P_DYNAMIC_ISLAND_ICON_VOLUME_MUTED=􀊡
 #
 ## Brightness Island config
 #
-export P_DYNAMIC_ISLAND_BRIGHTNESS_EXPAND_WIDTH=192 # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_BRIGHTNESS_EXPAND_WIDTH=192     # This value should match the notch size, appearence wise
 export P_DYNAMIC_ISLAND_BRIGHTNESS_MAX_EXPAND_WIDTH=250 # Max size when expanded
-export P_DYNAMIC_ISLAND_BRIGHTNESS_DEFAULT_HEIGHT=13
+export P_DYNAMIC_ISLAND_BRIGHTNESS_DEFAULT_HEIGHT=12
 export P_DYNAMIC_ISLAND_BRIGHTNESS_EXPAND_HEIGHT=16
 export P_DYNAMIC_ISLAND_BRIGHTNESS_CORNER_RAD=12 # Corner Radius
 export P_DYNAMIC_ISLAND_BRIGHTNESS_NORMAL_ICON_COLOR=0xffffffff
@@ -75,7 +77,7 @@ export P_DYNAMIC_ISLAND_ICON_BRIGHTNESS_HIGH=􀆭
 # Music Info
 export P_DYNAMIC_ISLAND_MUSIC_SOURCE="Music" # AVAILABLE OPTIONS (case sensitive): Music (apple music), Spotify
 export P_DYNAMIC_ISLAND_MUSIC_INFO_DEFAULT_HEIGHT=52
-export P_DYNAMIC_ISLAND_MUSIC_INFO_EXPAND_WIDTH=137 # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_MUSIC_INFO_EXPAND_WIDTH=137     # This value should match the notch size, appearence wise
 export P_DYNAMIC_ISLAND_MUSIC_INFO_MAX_EXPAND_WIDTH=270 # Max size when expanded#
 export P_DYNAMIC_ISLAND_MUSIC_INFO_EXPAND_HEIGHT=120
 export P_DYNAMIC_ISLAND_MUSIC_INFO_CORNER_RAD=34 # Corner Radius
@@ -85,20 +87,39 @@ export P_DYNAMIC_ISLAND_MUSIC_RESUME_MAX_EXPAND_WIDTH=295
 export P_DYNAMIC_ISLAND_MUSIC_RESUME_EXPAND_HEIGHT=65
 export P_DYNAMIC_ISLAND_MUSIC_RESUME_CORNER_RAD=15 # Corner Radius
 
+## WIFI Island config
+#
+export P_DYNAMIC_ISLAND_WIFI_EXPAND_WIDTH=150     # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_WIFI_MAX_EXPAND_WIDTH=295 # Max size when expanded
+export P_DYNAMIC_ISLAND_WIFI_EXPAND_HEIGHT=65
+export P_DYNAMIC_ISLAND_WIFI_CORNER_RAD=15 # Corner Radius
+export P_DYNAMIC_ISLAND_ICON_WIFI_CONNECTED=􀙇
+export P_DYNAMIC_ISLAND_ICON_WIFI_DISCONNECTED=􀙈
+
+#
+## Battery Island config
+#
+export P_DYNAMIC_ISLAND_BATTERY_EXPAND_WIDTH=150     # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_BATTERY_MAX_EXPAND_WIDTH=295 # Max size when expanded
+export P_DYNAMIC_ISLAND_BATTERY_EXPAND_HEIGHT=65
+export P_DYNAMIC_ISLAND_BATTERY_CORNER_RAD=15 # Corner Radius
+export P_DYNAMIC_ISLAND_ICON_BATTERY_CONNECTEDAC=􀢋
+export P_DYNAMIC_ISLAND_ICON_BATTERY_ONBATTERY=􀺸
+
 #
 ## Notification Island Config
 #
-export P_DYNAMIC_ISLAND_NOTIFICATION_EXPAND_WIDTH=107 # This value should match the notch size, appearence wise
+export P_DYNAMIC_ISLAND_NOTIFICATION_EXPAND_WIDTH=107     # This value should match the notch size, appearence wise
 export P_DYNAMIC_ISLAND_NOTIFICATION_MAX_EXPAND_WIDTH=280 # Max size when expanded
 export P_DYNAMIC_ISLAND_NOTIFICATION_EXPAND_HEIGHT=140
-export P_DYNAMIC_ISLAND_NOTIFICATION_CORNER_RAD=42 # Corner Radius
+export P_DYNAMIC_ISLAND_NOTIFICATION_CORNER_RAD=42        # Corner Radius
 export P_DYNAMIC_ISLAND_NOTIFICATION_MAX_ALLOWED_BODY=250 # Max allowed body for notification message
 
 ############# THESE VALUES SHOULDN'T BE UNTOUCHED #############
 
 #
 ## Colors
-# 
+#
 export P_DYNAMIC_ISLAND_COLOR_WHITE=0xffffffff
 export P_DYNAMIC_ISLAND_COLOR_BLACK=0xff000000
 export P_DYNAMIC_ISLAND_COLOR_TRANSPARENT=0x00000000
@@ -110,4 +131,4 @@ export P_DYNAMIC_ISLAND_COLOR_ICON_HIDDEN=$P_DYNAMIC_ISLAND_COLOR_BLACK
 export FONT="SF Pro" # Needs to have Regular, Bold, Semibold, Heavy and Black variants
 export NERD_FONT="Liga SFMono Nerd Font"
 
-export PADDINGS=3    # All paddings use this value (icon, label, background)
+export PADDINGS=3 # All paddings use this value (icon, label, background)
