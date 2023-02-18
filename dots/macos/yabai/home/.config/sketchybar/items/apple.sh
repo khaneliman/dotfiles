54867422c2c2810cdd34576b91c134f1e7ec253e
Austin Horstman <khaneliman12@gmail.com>
2023-02-18 08:49:10 -0600

    macos blue logo to match windows/arch setup

--- dots/macos/yabai/home/.config/sketchybar/items/apple.sh
+++ dots/macos/yabai/home/.config/sketchybar/items/apple.sh
@@ -6,7 +6,7 @@ POPUP_CLICK_SCRIPT="sketchybar --set \$NAME popup.drawing=toggle"
 sketchybar --add item           apple.logo left                             \
            --set apple.logo     icon="$APPLE"                               \
                                 icon.font="$FONT:Black:16.0"                \
-                                icon.color="$GREEN"                         \
+                                icon.color="$BLUE"                         \
                                 background.padding_right=15                 \
                                 label.drawing=off                           \
                                 click_script="$POPUP_CLICK_SCRIPT"          \
