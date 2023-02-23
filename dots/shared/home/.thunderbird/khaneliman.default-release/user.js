b4cf61bc2952bf3ef2a08760e4178787aa2e980e
Austin Horstman <khaneliman12@gmail.com>
2023-02-22 21:18:20 -0600

    shared include thunderbird config

---
+++ dots/shared/home/.thunderbird/khaneliman.default-release/user.js
@@ -0,0 +1,10 @@
+// Mozilla User Preferences
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.display.use_system_colors", true);
+user_pref("browser.theme.dark-toolbar-theme", true);
+user_pref("extensions.activeThemeID", "khaneliman12@gmail.com");
