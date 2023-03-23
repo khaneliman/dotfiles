81e6e71d7d908f4ef4ebdadcaf96c5d640731c1a
Austin Horstman <khaneliman12@gmail.com>
2023-03-22 21:32:52 -0500

    use existing neovim config

--- dots/nixos/modules/cli-apps/neovim/default.nix
+++ dots/nixos/modules/cli-apps/neovim/default.nix
@@ -12,16 +12,10 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      # @FIXME(jakehamilton): As of today (2022-12-09), `page` no longer works with my Neovim
-      # configuration. Either something in my configuration is breaking it or `page` is busted.
-      # page
       neovim
     ];
 
     environment.variables = {
-      # PAGER = "page";
-      # MANPAGER =
-      #   "page -C -e 'au User PageDisconnect sleep 100m|%y p|enew! |bd! #|pu p|set ft=man'";
       PAGER = "less";
       MANPAGER = "less";
       NPM_CONFIG_PREFIX = "$HOME/.npm-global";
@@ -30,7 +24,7 @@ in
 
     plusultra.home = {
       configFile = {
-        "dashboard-nvim/.keep".text = "";
+        "nvim/init.lua".text = "$HOME/.config/.dotfiles/dots/shared/home/.config/nvim/init.lua";
       };
 
       extraOptions = {
