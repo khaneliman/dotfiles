34c25608ec78ca0b3e59bc32d0649e84210d1196
Austin Horstman <khaneliman12@gmail.com>
2023-03-23 16:21:07 -0500

    test something

--- dots/nixos/modules/cli-apps/neovim/default.nix
+++ dots/nixos/modules/cli-apps/neovim/default.nix
@@ -1,38 +1,62 @@
-inputs@{ options, config, lib, pkgs, ... }:
+# inputs@{ options, config, lib, pkgs, ... }:
 
-with lib;
-with lib.internal;
-let
-  cfg = config.khaneliman.cli-apps.neovim;
-in
+# with lib;
+# with lib.internal;
 {
-  options.khaneliman.cli-apps.neovim = with types; {
-    enable = mkBoolOpt false "Whether or not to enable neovim.";
-  };
-
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      neovim
-    ];
-
-    environment.variables = {
-      PAGER = "less";
-      MANPAGER = "less";
-      NPM_CONFIG_PREFIX = "$HOME/.npm-global";
-      EDITOR = "nvim";
-    };
-
-    khaneliman.home = {
-      configFile = {
-        "nvim/init.lua".text = "$HOME/.config/.dotfiles/dots/shared/home/.config/nvim/init.lua";
-      };
-
-      extraOptions = {
-        # Use Neovim for Git diffs.
-        programs.zsh.shellAliases.vimdiff = "nvim -d";
-        programs.bash.shellAliases.vimdiff = "nvim -d";
-        programs.fish.shellAliases.vimdiff = "nvim -d";
-      };
+  inputs = {
+    options = inputs.options;
+    config = inputs.config;
+    lib = inputs.lib;
+    pkgs = inputs.pkgs;
+    dotfiles.url = "github:khaneliman/dotfiles";
+    nvim = {
+      type = "git";
+      url = "${inputs.dotfiles}/dots/shared/home/.config/nvim";
+      ref = "main";
+      path = "nvim";
     };
   };
+  outputs = { self, nixpkgs, dotfiles, nvim }:
+    let
+      cfg = config.khaneliman.cli-apps.neovim;
+      config = {
+        home.file.".config/nvim/init.vim".text = ''
+          " Your Neovim configuration here "
+        '';
+      };
+    in
+      if cfg.enable then {
+        environment.systemPackages = with pkgs; [
+          neovim
+        ];
+
+        environment.variables = {
+          PAGER = "less";
+          MANPAGER = "less";
+          NPM_CONFIG_PREFIX = "$HOME/.npm-global";
+          EDITOR = "nvim";
+        };
+
+        nixpkgs.config.home = {
+          configFile = {
+            "nvim/init.lua".text = "${dotfiles}/${nvim}/init.lua";
+          };
+
+          extraOptions = {
+            # Use Neovim for Git diffs.
+            programs.zsh.shellAliases.vimdiff = "nvim -d";
+            programs.bash.shellAliases.vimdiff = "nvim -d";
+            programs.fish.shellAliases.vimdiff = "nvim -d";
+          };
+        };
+      } else {
+        nixpkgs.config.home = {
+          configFile = {
+            "nvim/init.vim".text = ''
+              " Your default Neovim configuration here "
+            '';
+          };
+        };
+      }
 }
+
