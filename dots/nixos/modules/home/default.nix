f45030db1ac2c93bdce8105cdd6a5ae633bfad1e
Austin Horstman <khaneliman12@gmail.com>
2023-04-29 15:30:37 -0500

    add home-manager pkg

--- dots/nixos/modules/home/default.nix
+++ dots/nixos/modules/home/default.nix
@@ -1,30 +1,34 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  inputs,
-  ...
+{ options
+, config
+, pkgs
+, lib
+, inputs
+, ...
 }:
 with lib;
 with lib.internal; let
   cfg = config.khanelinix.home;
-in {
+in
+{
   imports = with inputs; [
     home-manager.nixosModules.home-manager
   ];
 
   options.khanelinix.home = with types; {
     file =
-      mkOpt attrs {}
-      "A set of files to be managed by home-manager's <option>home.file</option>.";
+      mkOpt attrs { }
+        "A set of files to be managed by home-manager's <option>home.file</option>.";
     configFile =
-      mkOpt attrs {}
-      "A set of files to be managed by home-manager's <option>xdg.configFile</option>.";
-    extraOptions = mkOpt attrs {} "Options to pass directly to home-manager.";
+      mkOpt attrs { }
+        "A set of files to be managed by home-manager's <option>xdg.configFile</option>.";
+    extraOptions = mkOpt attrs { } "Options to pass directly to home-manager.";
   };
 
   config = {
+    environment.systemPackages = with pkgs; [
+      home-manager
+    ];
+
     khanelinix.home.extraOptions = {
       home.stateVersion = config.system.stateVersion;
       home.file = mkAliasDefinitions options.khanelinix.home.file;
