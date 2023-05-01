3ca024b51e023080975fbccdef9865655a9c0767
Austin Horstman <khaneliman12@gmail.com>
2023-05-01 13:54:25 -0500

    nix add cachix

--- dots/nixos/modules/nix/default.nix
+++ dots/nixos/modules/nix/default.nix
@@ -1,14 +1,14 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  ...
+{ options
+, config
+, pkgs
+, lib
+, ...
 }:
 with lib;
 with lib.internal; let
   cfg = config.khanelinix.nix;
-in {
+in
+{
   options.khanelinix.nix = with types; {
     enable = mkBoolOpt true "Whether or not to manage nix configuration.";
     package = mkOpt package pkgs.nixUnstable "Which nix package to use.";
@@ -21,39 +21,50 @@ in {
       alejandra
       nix-index
       nix-prefetch-git
+      cachix
     ];
 
-    nix = let
-      users = ["root" config.khanelinix.user.name];
-    in {
-      package = cfg.package;
+    nix =
+      let
+        users = [ "root" config.khanelinix.user.name ];
+      in
+      {
+        package = cfg.package;
 
-      settings =
-        {
-          experimental-features = "nix-command flakes";
-          http-connections = 50;
-          warn-dirty = false;
-          log-lines = 50;
-          sandbox = "relaxed";
-          auto-optimise-store = true;
-          trusted-users = users;
-          allowed-users = users;
-        }
-        // (lib.optionalAttrs config.khanelinix.tools.direnv.enable {
-          keep-outputs = true;
-          keep-derivations = true;
-        });
+        settings =
+          {
+            experimental-features = "nix-command flakes";
+            http-connections = 50;
+            warn-dirty = false;
+            log-lines = 50;
+            sandbox = "relaxed";
+            auto-optimise-store = true;
+            trusted-users = users;
+            allowed-users = users;
+            substituters = [
+              "https://hyprland.cachix.org"
+              "https://nix-community.cachix.org"
+            ];
+            trusted-public-keys = [
+              "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            ];
+          }
+          // (lib.optionalAttrs config.khanelinix.tools.direnv.enable {
+            keep-outputs = true;
+            keep-derivations = true;
+          });
 
-      gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 30d";
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 30d";
+        };
+
+        # flake-utils-plus
+        generateRegistryFromInputs = true;
+        generateNixPathFromInputs = true;
+        linkInputs = true;
       };
-
-      # flake-utils-plus
-      generateRegistryFromInputs = true;
-      generateNixPathFromInputs = true;
-      linkInputs = true;
-    };
   };
 }
