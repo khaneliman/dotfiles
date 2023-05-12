aff4e405af55f70ada43f5ef6b96d115bfcf088a
Austin Horstman <khaneliman12@gmail.com>
2023-05-11 23:22:53 -0500

    cleanup unused themes and aconfmgr attempt

--- dots/linux/hyprland/home/.config/aconfmgr/20-social.sh
+++
@@ -1,3 +0,0 @@
-AddPackage telegram-desktop # Official Telegram Desktop client
-AddPackage discord          # All-in-one voice and text chat for gamers
-AddPackage zoom             # Video Conferencing and Web Conferencing Service
--- dots/linux/hyprland/home/.config/aconfmgr/20-virtualization.sh
+++
@@ -1,9 +0,0 @@
-AddPackage qemu-emulators-full # All QEMU user mode and system emulators
-AddPackage qemu-full           # A full QEMU setup
-AddPackage qemu-guest-agent    # QEMU Guest Agent
-AddPackage edk2-ovmf           # Firmware for Virtual Machines (x86_64, i686)
-AddPackage virt-manager        # Desktop user interface for managing virtual machines
-AddPackage vde2                # Virtual Distributed Ethernet for emulators like qemu
-
-AddPackage virt-viewer
-AddPackage bridge-utils
